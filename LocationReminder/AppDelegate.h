1f1c765d3a5e6ddd6def215c7b8fdd86dfde8f2a
Rio <jayb967@gmail.com>
2017-05-01 10:47:01 -0700

    Initial Commit

---
+++ LocationReminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminder
+//
+//  Created by Rio Balderas on 5/1/17.
+//  Copyright © 2017 Jay Balderas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationReminder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationReminder
+//
+//  Created by Rio Balderas on 5/1/17.
+//  Copyright © 2017 Jay Balderas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
